8beb0eb75a8f210378d35b85fa37ad7da929da90
Anushkaunune <153426598+Anushkaunune@users.noreply.github.com>
2023-12-10 02:58:45 -0800

    Add files via upload

---
+++ Iteration2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int i = 0;
+    int Count = 10;
+
+    printf("Enter number of times yoy want to display Jay Ganesh on screen..\n");
+
+    for( i= 1; i <= Count; i++)
+    {
+        printf("Jay Ganesh..\n")
+    }
+
+    return 0;
+}
